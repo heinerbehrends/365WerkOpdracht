eeba8f09e7f979ef79cbfc05419f105574e4e403
heinerbehrends <heinerbehrends@hotmail.com>
2021-09-27 14:48:12 +0200

    Added the logo

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,19 @@
 <template>
-  <LoginForm/>
+  <div>
+    <img src="/logo.svg" alt="365werk logo" class="logo">
+    <LoginForm/>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style lang="scss" scoped>
+  .logo {
+    width: 320px;
+    margin: 0 auto;
+    display: block;
+    padding: $xl
+  }
+</style>
