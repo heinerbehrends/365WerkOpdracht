4210fa3d5641904542ba62b9e2cc2ecf2d5a55d3
heinerbehrends <heinerbehrends@hotmail.com>
2021-09-27 12:20:47 +0200

    Added handling of 401 error, pending request

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -1,6 +1,9 @@
 <template>
   <form class="form" @submit.prevent="submitForm(form)">
     <h1 class="heading">Log in</h1>
+    <p v-if="error === 401" class="error">
+      De combinatie van e-mailadres en wachtwoord is niet geldig. 
+    </p>
     <label class="column label">
       E-mailadres 
       <input
@@ -25,12 +28,12 @@
         aria-required="true" 
       >
     </label>
-    <Button text="Log in" />
+    <Button text="Log in" :disabled="isPending" />
   </form>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions, mapState } from 'vuex'
 import Button from './Button.vue'
 
 export default {
@@ -40,8 +43,14 @@ export default {
       form: {
         email: null,
         password: null,
-      }
+      },
     }
+  },
+  computed: {
+    ...mapState({
+      isPending: state => state.isPending,
+      error: state => state.error,
+    })
   },
   methods: {
     ...mapActions(['submitForm']),
@@ -70,5 +79,11 @@ export default {
   .input {
     padding: 0.5rem 0.75rem;
     margin-top: 0.25rem;
+  }
+  .error {
+    padding: 0.5rem 0.75rem;
+    border: 1px solid red;
+    background-color: #fcc9c9;
+    border-radius: 0.5rem;
   }
 </style>
--- store/index.js
+++ store/index.js
@@ -1,12 +1,20 @@
 export const state = () => ({
   loggedIn: false,
   user: null,
+  isPending: false,
+  error: null,
 })
 
 export const mutations = {
+  togglePending(state) {
+    state.isPending = !state.isPending
+  },
   init(state, user) {
     state.user = user
     state.loggedIn = true
+  },
+  error(state, error) {
+    state.error = error
   },
   logOut(state) {
     state.user = null
@@ -16,13 +24,24 @@ export const mutations = {
 
 export const actions = {
   async submitForm({ commit }, form) {
+    commit('togglePending')
     const response = await fetch(
       'https://login-opdracht.365werk.workers.dev/login',
       {
         method: 'POST',
         body: JSON.stringify(form),
       }
-    ).then((response) => response.json())
+    )
+      .then((response) => {
+        commit('togglePending')
+        if (response.status !== 200) {
+          commit('error', response.status)
+          throw new Error(response.status)
+        }
+        return response.json()
+      })
+      .catch((error) => console.error(error))
+    if (typeof response === 'undefined') return
     commit('init', response)
     this.$router.push({ name: 'user' })
   },
